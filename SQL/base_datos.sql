72f76f2aef25f733c197c29d22b4110661263de5
One <luizcortesj@gmail.com>
2017-04-26 21:43:42 -0600

    DB: Agregando tabla de usuarios y roles

--- SQL/base_datos.sql
+++ SQL/base_datos.sql
@@ -33,3 +33,74 @@
 -- @since	Version 1.0.0
 -- @filesource
 --
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id_roles` int(11) NOT NULL,
+  `rol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_roles`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `roles` */
+
+LOCK TABLES `roles` WRITE;
+
+insert  into `roles`(`id_roles`,`rol`) values (0,'Administrador'),(1,'Gerente Restaurante'),(2,'Cocinero'),(3,'Salonero'),(4,'Cliente');
+
+UNLOCK TABLES;
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `cedula` varchar(15) NOT NULL,
+  `usuario` varchar(20) DEFAULT NULL,
+  `clave` varchar(88) DEFAULT NULL,
+  `id_roles` int(11) DEFAULT NULL,
+  `inactivo` bit(1) DEFAULT b'0',
+  PRIMARY KEY (`cedula`),
+  UNIQUE KEY `UNIQUE` (`usuario`),
+  KEY `fk_rol` (`id_roles`),
+  CONSTRAINT `fk_cedula` FOREIGN KEY (`cedula`) REFERENCES `usuario_datos` (`cedula`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_roles` FOREIGN KEY (`id_roles`) REFERENCES `roles` (`id_roles`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `usuario` */
+
+LOCK TABLES `usuario` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `usuario_datos` */
+
+DROP TABLE IF EXISTS `usuario_datos`;
+
+CREATE TABLE `usuario_datos` (
+  `cedula` varchar(15) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `apellido` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`cedula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `usuario_datos` */
+
+LOCK TABLES `usuario_datos` WRITE;
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
